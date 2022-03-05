9fecf9b42e9d669fe611417f4046103342635401
Raghul Tamilaiah <raghulkumar31960@gmail.com>
2022-03-05 21:27:40 +0530

    added the changes

--- src/main/kotlin/com/raghul/expenseservice/ExpenseserviceApplication.kt
+++ src/main/kotlin/com/raghul/expenseservice/ExpenseserviceApplication.kt
@@ -1,11 +1,16 @@
 package com.raghul.expenseservice
 
+import com.raghul.expenseservice.model.Transaction
+import com.raghul.expenseservice.repository.TransactionRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 class ExpenseserviceApplication
 
 fun main(args: Array<String>) {
+	var context = runApplication<ExpenseserviceApplication>(*args)
 
 
 }
